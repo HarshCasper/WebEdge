fcfb8df50064b45d269f07e1aac8508dd50fa576
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2020-11-22 17:32:30 +0530

    Feat: Added setup.py and made a Package (#19)

--- webedge/webedge.py
+++ webedge/webedge.py
@@ -1,7 +1,7 @@
 import sys
-import website_analysis
+from webedge import website_analysis
 
-if __name__ == '__main__':
+def main():
     if len(sys.argv) == 2:
         site_domain = sys.argv[1]
         spider = website_analysis.Spider(site_domain)
@@ -12,5 +12,9 @@ if __name__ == '__main__':
         spider = website_analysis.spider(site_domain, site_map)
         spider.crawl()
     else:
-        print("Run the Command: python3 webedge/main.py http://[YOUR_WEBSITE]]")
+        print("Run the Command: webedge http://[YOUR_WEBSITE]]")
         exit
+
+if __name__ == "__main__":
+    sys.exit(main())
+    
