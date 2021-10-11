fb18ee82def4e54e5d7e3b684788eb2c52bc4cf6
harshcasper <hmishra@redhat.com>
2021-10-11 12:28:27 +0530

    CI: Add support for Python-3.10

--- setup.py
+++ setup.py
@@ -1,25 +1,44 @@
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     REQUIREMENTS = f.read().splitlines()
-with open('README.md', encoding='utf8') as f:
+with open("README.md", encoding="utf8") as f:
     README = f.read()
 
 setup(
-    name='WebEdge',
-    version='1.0.2',
-    license='MIT License',
-    author='MLH Fellowship Team 1',
-    author_email='erbeusgriffincasper@gmail.com',
-    description='Bringing Edge to your Web Performance',
+    name="WebEdge",
+    version="1.0.2",
+    license="MIT License",
+    author="MLH Fellowship Team 1",
+    author_email="erbeusgriffincasper@gmail.com",
+    description="Bringing Edge to your Web Performance",
     long_description=README,
-    long_description_content_type='text/markdown',
-    url='https://github.com/HarshCasper/WebEdge',
+    long_description_content_type="text/markdown",
+    url="https://github.com/HarshCasper/WebEdge",
     install_requires=REQUIREMENTS,
-    packages=find_packages(exclude = ["*.tests", "*.tests.*", "tests.*", "tests"]),
-    entry_points={
-        'console_scripts': [
-            'webedge = webedge.webedge:main'
-        ]
-    }
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    entry_points={"console_scripts": ["webedge = webedge.webedge:main"]},
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Browsers",
+        "Topic :: Internet :: WWW/HTTP :: Indexing/Search",
+        "Topic :: Internet :: WWW/HTTP :: Site Management",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: System :: Monitoring",
+    ],
 )
